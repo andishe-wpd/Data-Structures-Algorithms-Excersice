f95095ad4fe8ba9caeee637642581a08c07b0342
Andishe Abbasian <andishdev@gmail.com>
2025-02-11 10:30:38 +0330

    Feat:just-update-the-order

--- 0-FindNemo/1-performanceMesurement.js
+++ 0-FindNemo/1-performanceMesurement.js
@@ -2,11 +2,24 @@ let rickAndMortyGuys = ["Rick", "Morty", "Summer", "Jerry", "Beth"];
 const rickClones = new Array(10000).fill("RICKCLONE");
 rickAndMortyGuys = rickClones.concat(rickAndMortyGuys);
 
+function findJerryWithRegularFunction(arr) {
+  const timMesureStart = performance.now();
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "Jerry") {
+      console.log(`Regular found Jerry in ${i + 1} place of array`);
+      break;
+    }
+  }
+  const timeMesureEnd = performance.now();
+  console.log(
+    `the operation took ${timeMesureEnd - timMesureStart} milliseconds`
+  );
+}
 const findJerryWithArrowFunction = (arr) => {
   const timMesureStart = performance.now();
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === "Jerry") {
-      console.log(`found Jerry in ${i + 1} place of array`);
+      console.log(`Arrow found Jerry in ${i + 1} place of array`);
       break;
     }
   }
@@ -16,19 +29,5 @@ const findJerryWithArrowFunction = (arr) => {
   );
 };
 
-function findJerryWithRegularFunction(arr) {
-  const timMesureStart = performance.now();
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === "Jerry") {
-      console.log(`found Jerry in ${i + 1} place of array`);
-      break;
-    }
-  }
-  const timeMesureEnd = performance.now();
-  console.log(
-    `the operation took ${timeMesureEnd - timMesureStart} milliseconds`
-  );
-}
-
-findJerryWithArrowFunction(rickAndMortyGuys);
 findJerryWithRegularFunction(rickAndMortyGuys);
+findJerryWithArrowFunction(rickAndMortyGuys);
