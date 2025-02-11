4fb6ef42a02221a27a89301f2c84344c61e5c1d8
Andishe Abbasian <andishdev@gmail.com>
2025-02-11 14:42:58 +0330

    Feat:bigO-for-a-loog

---
+++ 0-FindNemo/2-stepMesurement.js
@@ -0,0 +1,19 @@
+let rickAndMortyGuys = ["Rick", "Morty", "Summer", "Jerry", "Beth"];
+const rickClones = new Array(100).fill("RICKCLONE");
+rickAndMortyGuys = rickClones.concat(rickAndMortyGuys);
+
+const findJerry = (arr) => {
+  let stepCounter = 0;
+  for (let i = 0; i < arr.length; i++) {
+    stepCounter++;
+    if (arr[i] === "Jerry") {
+      console.log(`Arrow found Jerry in ${i + 1} place of array`);
+    }
+  }
+  console.log(`stepCounter is ${stepCounter}`);
+};
+
+findJerry(rickAndMortyGuys);
+
+// now the big o is O(n) cause we have to loop through the array
+// and the stepCounter is O(1) cause we have to count the steps
